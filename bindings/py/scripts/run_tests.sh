db38d58bfb6bf53472d01eb7dcabf1e98006a7d6
Andrius Pukšta <andrius.puksta@sensmetry.com>
2025-10-09 16:43:07 +0300

    remove old Python path workaround
    Signed-off-by: Andrius Pukšta <andrius.puksta@sensmetry.com>

--- bindings/py/scripts/run_tests.sh
+++ bindings/py/scripts/run_tests.sh
@@ -8,13 +8,11 @@ PACKAGE_DIR=$(dirname $SCRIPT_DIR)
 
 cd $PACKAGE_DIR
 
-export LD_LIBRARY_PATH=$(python -c "from sysconfig import get_config_var as s; print(s(\"LIBDIR\"))")
-
 # Workaround to make pyo3 detect venv python libs on macOS.
 # See https://github.com/PyO3/pyo3/issues/1741
 export PYTHONHOME="$(python -c 'import sys; print(sys.base_prefix)')"
 
-maturin develop
+uv run maturin develop
 
 cargo test --no-default-features
 
