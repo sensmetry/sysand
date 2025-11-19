848080081e04e3420507224e5edc8d15caf1fb18
victor.linroth.sensmetry <victor.linroth@sensmetry.com>
2025-11-19 09:35:03 +0100

    Added derive macro for `ProjectMut` trait.
    Signed-off-by: victor.linroth.sensmetry <victor.linroth@sensmetry.com>

--- core/tests/project_derive.rs
+++ core/tests/project_derive.rs
@@ -1,70 +1,55 @@
 // SPDX-FileCopyrightText: © 2025 Sysand contributors <opensource@sensmetry.com>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use std::collections::HashMap;
-use std::io::Read;
-use std::str::FromStr;
+use std::{
+    collections::HashMap,
+    io::{Cursor, Read},
+};
 
-#[cfg(feature = "filesystem")]
-use camino::Utf8PathBuf;
-use typed_path::Utf8UnixPathBuf;
-
-#[cfg(feature = "filesystem")]
-use sysand_core::project::local_src::LocalSrcProject;
-use sysand_core::project::{ProjectRead, memory::InMemoryProject};
-
-// Currently these need to be in scope for derive(ProjectRead) to work
-// TODO: Include these types in ProjectRead trait
-use sysand_core::lock::Source;
-use sysand_core::model::{InterchangeProjectInfoRaw, InterchangeProjectMetadataRaw};
+use sysand_core::{
+    model::{InterchangeProjectInfoRaw, InterchangeProjectMetadataRaw},
+    project::{ProjectMut, ProjectRead, memory::InMemoryProject},
+};
 
 #[derive(ProjectRead)]
-enum SingleVariantEnum {
-    Memory(InMemoryProject),
+enum OneVariantProjectRead {
+    Variant(InMemoryProject),
 }
 
-#[cfg(feature = "filesystem")]
 #[derive(ProjectRead)]
-enum DoubleVariantEnum {
-    Memory(InMemoryProject),
-    LocalSrc(LocalSrcProject),
+enum TwoVariantProjectRead {
+    First(InMemoryProject),
+    Second(InMemoryProject),
+}
+
+#[derive(ProjectRead, ProjectMut)]
+enum OneVariantProjectMut {
+    Variant(InMemoryProject),
 }
 
 #[test]
-fn test_macro_single() {
-    let _test_single = SingleVariantEnum::Memory(InMemoryProject::new());
-}
-
-#[cfg(feature = "filesystem")]
-#[test]
-fn test_macro_double() {
-    let _test_double = DoubleVariantEnum::LocalSrc(LocalSrcProject {
-        project_path: Utf8PathBuf::new(),
-    });
+fn test_macro_one_variant() {
+    let _project = OneVariantProjectRead::Variant(InMemoryProject::new());
 }
 
 #[test]
-fn test_error_display_single() {
-    let error = <SingleVariantEnum as ProjectRead>::Error::Memory(
-        <InMemoryProject as ProjectRead>::Error::AlreadyExists("file".to_string()),
+fn test_macro_two_variants() {
+    let _project_first = TwoVariantProjectRead::First(InMemoryProject::new());
+    let _project_second = TwoVariantProjectRead::Second(InMemoryProject::new());
+}
+
+#[test]
+fn test_error_to_string() {
+    let error = <OneVariantProjectRead as ProjectRead>::Error::Variant(
+        <InMemoryProject as ProjectRead>::Error::AlreadyExists("project".to_string()),
     );
-    let _display = format!("{}", error);
+    let _string = error.to_string();
 }
 
-#[cfg(feature = "filesystem")]
 #[test]
-fn test_error_display_double() {
-    let error = <DoubleVariantEnum as ProjectRead>::Error::LocalSrc(
-        <LocalSrcProject as ProjectRead>::Error::AlreadyExists("file".to_string()),
-    );
-    let _display = format!("{}", error);
-}
-
-#[cfg(feature = "filesystem")]
-#[test]
-fn test_macro_double_get_project() {
+fn test_macro_get_project() {
     let info = InterchangeProjectInfoRaw {
-        name: "single_get_info".to_string(),
+        name: "get_project".to_string(),
         description: None,
         version: "1.2.3".to_string(),
         license: None,
@@ -81,7 +66,7 @@ fn test_macro_double_get_project() {
         includes_implied: None,
         checksum: None,
     };
-    let test_double = DoubleVariantEnum::Memory(InMemoryProject {
+    let test_double = OneVariantProjectRead::Variant(InMemoryProject {
         info: Some(info.clone()),
         meta: Some(meta.clone()),
         files: HashMap::new(),
@@ -92,7 +77,38 @@ fn test_macro_double_get_project() {
 }
 
 #[test]
-fn test_macro_single_get_info() {
+fn test_macro_read_source() {
+    let mut files = HashMap::new();
+    let path = "path";
+    let file_content = "file content".to_string();
+    files.insert(path.into(), file_content.clone());
+    let project = OneVariantProjectRead::Variant(InMemoryProject {
+        info: None,
+        meta: None,
+        files,
+        nominal_sources: vec![],
+    });
+
+    let mut buffer = String::new();
+
+    project
+        .read_source(path)
+        .unwrap()
+        .read_to_string(&mut buffer)
+        .unwrap();
+
+    assert_eq!(buffer, file_content);
+}
+
+#[test]
+fn test_macro_sources() {
+    let project = OneVariantProjectRead::Variant(InMemoryProject::new());
+
+    assert_eq!(project.sources(), vec![]);
+}
+
+#[test]
+fn test_macro_put_info() {
     let info = InterchangeProjectInfoRaw {
         name: "single_get_info".to_string(),
         description: None,
@@ -103,18 +119,17 @@ fn test_macro_single_get_info() {
         topic: vec![],
         usage: vec![],
     };
-    let test_single = SingleVariantEnum::Memory(InMemoryProject {
-        info: Some(info.clone()),
-        meta: None,
-        files: HashMap::new(),
-        nominal_sources: vec![],
-    });
+    let mut project = OneVariantProjectMut::Variant(InMemoryProject::new());
 
-    assert_eq!(test_single.get_info().unwrap().unwrap(), info);
+    assert!(project.get_info().unwrap().is_none());
+
+    project.put_info(&info, false).unwrap();
+
+    assert_eq!(project.get_info().unwrap().unwrap(), info);
 }
 
 #[test]
-fn test_macro_single_get_meta() {
+fn test_macro_put_meta() {
     let meta = InterchangeProjectMetadataRaw {
         index: indexmap::IndexMap::new(),
         created: "0000-00-00T00:00:00.123456789Z".to_string(),
@@ -123,43 +138,37 @@ fn test_macro_single_get_meta() {
         includes_implied: None,
         checksum: None,
     };
-    let test_single = SingleVariantEnum::Memory(InMemoryProject {
+    let mut project = OneVariantProjectMut::Variant(InMemoryProject::new());
+
+    assert!(project.get_meta().unwrap().is_none());
+
+    project.put_meta(&meta, false).unwrap();
+
+    assert_eq!(project.get_meta().unwrap().unwrap(), meta);
+}
+
+#[test]
+fn test_macro_write_source() {
+    let path = "path";
+    let file_content = "file content".to_string();
+    let mut project = OneVariantProjectMut::Variant(InMemoryProject {
         info: None,
-        meta: Some(meta.clone()),
+        meta: None,
         files: HashMap::new(),
         nominal_sources: vec![],
     });
 
-    assert_eq!(test_single.get_meta().unwrap().unwrap(), meta);
-}
-
-#[test]
-fn test_macro_single_read_source() {
-    let mut map = HashMap::new();
-    let path = Utf8UnixPathBuf::from_str("path").unwrap();
-    let file_content = "file content".to_string();
-    map.insert(path.clone(), file_content.clone());
-    let test_single = SingleVariantEnum::Memory(InMemoryProject {
-        info: None,
-        meta: None,
-        files: map,
-        nominal_sources: vec![],
-    });
+    project
+        .write_source(path, &mut Cursor::new(file_content.as_str()), false)
+        .unwrap();
 
     let mut buffer = String::new();
 
-    test_single
+    project
         .read_source(path)
         .unwrap()
         .read_to_string(&mut buffer)
         .unwrap();
 
     assert_eq!(buffer, file_content);
-}
-
-#[test]
-fn test_macro_single_sources() {
-    let test_single = SingleVariantEnum::Memory(InMemoryProject::new());
-
-    assert_eq!(test_single.sources(), vec![]);
 }
