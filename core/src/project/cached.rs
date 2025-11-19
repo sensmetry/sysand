76c0a839819eff6a4c81e9ba2385d4f45b0175c3
victor.linroth.sensmetry <victor.linroth@sensmetry.com>
2025-11-19 10:55:02 +0100

    Added generic `CachedProject`.
    Signed-off-by: victor.linroth.sensmetry <victor.linroth@sensmetry.com>

---
+++ core/src/project/cached.rs
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: © 2025 Sysand contributors <opensource@sensmetry.com>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use crate::project::{ProjectRead, Utf8UnixPath};
+
+#[derive(Clone, Debug)]
+pub struct CachedProject<Local: ProjectRead, Remote: ProjectRead> {
+    local: Local,
+    remote: Remote,
+}
+
+impl<Local: ProjectRead, Remote: ProjectRead> CachedProject<Local, Remote> {
+    pub fn new(local: Local, remote: Remote) -> Self {
+        CachedProject::<Local, Remote> { local, remote }
+    }
+}
+
+impl<Local: ProjectRead, Remote: ProjectRead> ProjectRead for CachedProject<Local, Remote> {
+    type Error = Local::Error;
+
+    fn get_project(
+        &self,
+    ) -> Result<
+        (
+            Option<crate::model::InterchangeProjectInfoRaw>,
+            Option<crate::model::InterchangeProjectMetadataRaw>,
+        ),
+        Self::Error,
+    > {
+        self.local.get_project()
+    }
+
+    type SourceReader<'a>
+        = Local::SourceReader<'a>
+    where
+        Self: 'a;
+
+    fn read_source<P: AsRef<Utf8UnixPath>>(
+        &self,
+        path: P,
+    ) -> Result<Self::SourceReader<'_>, Self::Error> {
+        self.local.read_source(path)
+    }
+
+    fn sources(&self) -> Vec<crate::lock::Source> {
+        self.remote.sources()
+    }
+}
