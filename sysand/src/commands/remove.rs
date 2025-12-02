4a99d48caa5a67515585153072fb63f0ced178e7
victor.linroth.sensmetry <victor.linroth@sensmetry.com>
2025-12-02 14:45:02 +0100

    Provide command line options for source configuration when adding usages.
    Signed-off-by: victor.linroth.sensmetry <victor.linroth@sensmetry.com>

--- sysand/src/commands/remove.rs
+++ sysand/src/commands/remove.rs
@@ -2,12 +2,17 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use anyhow::Result;
-use sysand_core::{project::local_src::LocalSrcProject, remove::do_remove};
+use sysand_core::{
+    config::local_fs::remove_project_source_from_config, project::local_src::LocalSrcProject,
+    remove::do_remove,
+};
 
 use crate::CliError;
 
 pub fn command_remove(iri: String, current_project: Option<LocalSrcProject>) -> Result<()> {
     let mut current_project = current_project.ok_or(CliError::MissingProjectCurrentDir)?;
+
+    remove_project_source_from_config(current_project.root_path(), &iri)?;
 
     let usages = do_remove(&mut current_project, &iri)?;
 
