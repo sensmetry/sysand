a92629245562c347e67fee4306497fef388bafbb
Andrius Pukšta <andrius.puksta@sensmetry.com>
2025-12-15 16:13:10 +0200

    refactor lock command to make its parts reusable
    Signed-off-by: Andrius Pukšta <andrius.puksta@sensmetry.com>

--- sysand/src/commands/lock.rs
+++ sysand/src/commands/lock.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use std::path::Path;
 use std::sync::Arc;
 
-use anyhow::{Result, bail};
+use anyhow::{Result, anyhow};
 use pubgrub::Reporter as _;
 
 use sysand_core::{
@@ -13,11 +13,12 @@ use sysand_core::{
         DEFAULT_LOCKFILE_NAME, LockError, LockOutcome, LockProjectError, do_lock_local_editable,
     },
     config::Config,
-    project::utils::wrapfs,
+    project::{ProjectRead, memory::InMemoryProject, utils::wrapfs},
     resolve::{
+        ResolveRead,
         memory::{AcceptAll, MemoryResolver},
         priority::PriorityResolver,
-        standard::standard_resolver,
+        standard::{StandardResolver, standard_resolver},
     },
     solve::pubgrub::{DependencyIdentifier, InternalSolverError},
     stdlib::known_std_libs,
@@ -35,17 +36,62 @@ pub fn command_lock<P: AsRef<Path>>(
     config: &Config,
     client: reqwest_middleware::ClientWithMiddleware,
     runtime: Arc<tokio::runtime::Runtime>,
-) -> Result<()> {
+) -> Result<sysand_core::lock::Lock> {
     assert!(path.as_ref().is_relative(), "{}", path.as_ref().display());
+
+    let provided_iris = if !resolution_opts.include_std {
+        known_std_libs()
+    } else {
+        HashMap::default()
+    };
+    let wrapped_resolver = create_resolver(
+        &path,
+        resolution_opts,
+        config,
+        provided_iris,
+        client,
+        runtime,
+    )?;
+
+    let LockOutcome {
+        lock,
+        dependencies: _dependencies,
+    } = match do_lock_local_editable(&path, wrapped_resolver) {
+        Ok(lock_outcome) => lock_outcome,
+        Err(LockProjectError::LockError(lock_error)) => {
+            return Err(handle_lock_error(lock_error));
+        }
+        Err(err) => Err(err)?,
+    };
+
+    let canonical = lock.canonicalize();
+    wrapfs::write(
+        path.as_ref().join(DEFAULT_LOCKFILE_NAME),
+        canonical.to_string(),
+    )?;
+
+    Ok(canonical)
+}
+
+pub fn create_resolver<P: AsRef<Path>>(
+    path: &P,
+    resolution_opts: ResolutionOptions,
+    config: &Config,
+    provided_iris: HashMap<String, Vec<InMemoryProject>>,
+    client: reqwest_middleware::ClientWithMiddleware,
+    runtime: Arc<tokio::runtime::Runtime>,
+) -> Result<
+    PriorityResolver<MemoryResolver<AcceptAll, InMemoryProject>, StandardResolver>,
+    anyhow::Error,
+> {
     let ResolutionOptions {
         index,
         default_index,
         no_index,
-        include_std,
+        include_std: _,
     } = resolution_opts;
 
-    let cwd = wrapfs::current_dir().ok();
-
+    let cwd = wrapfs::current_dir()?;
     let local_env_path = path
         .as_ref()
         .join(sysand_core::env::local_directory::DEFAULT_ENV_NAME);
@@ -56,16 +102,11 @@ pub fn command_lock<P: AsRef<Path>>(
         Some(config.index_urls(index, vec![DEFAULT_INDEX_URL.to_string()], default_index)?)
     };
 
-    let provided_iris = if !include_std {
-        known_std_libs()
-    } else {
-        HashMap::default()
-    };
-
+    // TODO: add fn next to known_std_libs() to get this structure directly
+    // it is created in most? all? places where `known_std_libs()` is used
     let mut memory_projects = HashMap::default();
-
     for (k, v) in provided_iris {
-        memory_projects.insert(fluent_uri::Iri::parse(k.clone()).unwrap(), v.to_vec());
+        memory_projects.insert(fluent_uri::Iri::parse(k).unwrap(), v);
     }
 
     let wrapped_resolver = PriorityResolver::new(
@@ -74,7 +115,7 @@ pub fn command_lock<P: AsRef<Path>>(
             projects: memory_projects,
         },
         standard_resolver(
-            cwd,
+            Some(cwd),
             if local_env_path.is_dir() {
                 Some(local_env_path)
             } else {
@@ -85,64 +126,84 @@ pub fn command_lock<P: AsRef<Path>>(
             runtime,
         ),
     );
+    Ok(wrapped_resolver)
+}
 
-    let LockOutcome {
-        lock,
-        dependencies: _dependencies,
-    } = match do_lock_local_editable(&path, wrapped_resolver) {
-        Ok(lock_outcome) => lock_outcome,
-        Err(LockProjectError::LockError(lock_error)) => {
-            if let LockError::Solver(solver_error) = lock_error {
-                match *solver_error.inner {
-                    pubgrub::PubGrubError::NoSolution(mut derivation_tree) => {
-                        derivation_tree.collapse_no_versions();
-                        bail!(
-                            "Failed to satisfy usage constraints:\n{}",
-                            pubgrub::DefaultStringReporter::report(&derivation_tree)
-                        );
-                    }
-                    pubgrub::PubGrubError::ErrorRetrievingDependencies {
-                        package, source, ..
-                    } => match package {
-                        DependencyIdentifier::Requested(_) => {
-                            bail!("Unexpected internal error: {:?}", source)
-                        }
-                        DependencyIdentifier::Remote(iri) => {
-                            bail!("Failed to retrieve (transitive) usages of usage {}", iri)
-                        }
-                    },
-                    pubgrub::PubGrubError::ErrorChoosingVersion { package, source } => {
-                        match package {
-                            DependencyIdentifier::Requested(_) => {
-                                bail!("Unexpected internal error: {:?}", source)
-                            }
-                            DependencyIdentifier::Remote(iri) => {
-                                bail!("Unable to select version of usage {}", iri)
-                            }
-                        }
-                    }
-                    pubgrub::PubGrubError::ErrorInShouldCancel(err) => match err {
+pub fn handle_lock_error<PR: ProjectRead, RR: ResolveRead + std::fmt::Debug>(
+    lock_error: LockError<PR, RR>,
+) -> anyhow::Error {
+    if let LockError::Solver(solver_error) = lock_error {
+        match *solver_error.inner {
+            pubgrub::PubGrubError::NoSolution(mut derivation_tree) => {
+                derivation_tree.collapse_no_versions();
+
+                anyhow!(
+                    "failed to satisfy usage constraints:\n{}",
+                    pubgrub::DefaultStringReporter::report(&derivation_tree)
+                )
+            }
+            pubgrub::PubGrubError::ErrorRetrievingDependencies {
+                package, source, ..
+            } => match package {
+                DependencyIdentifier::Requested(_) => {
+                    match source {
                         InternalSolverError::Resolution(err) => {
-                            bail! {"Resolution error: {:?}", err}
+                            anyhow!("resolution error: {:?}", err)
                         }
                         // InternalSolverError::InvalidProject => {
-                        //     bail!("Found invalid project during usage resolution")
+                        //     anyhow!("found invalid project during usage resolution")
                         // }
                         InternalSolverError::NotResolvable(iri) => {
-                            bail!("Unable to resolve usage '{}'", iri)
+                            anyhow!("unable to resolve usage `{}`", iri)
                         }
-                    },
+                        InternalSolverError::VersionNotAvailable(msg) => {
+                            anyhow!("requested version unavailable: {msg}")
+                        }
+                    }
+                    // anyhow!("unexpected internal error: {:?}", source)
                 }
-            }
-            Err(lock_error)?
+                DependencyIdentifier::Remote(iri) => {
+                    anyhow!("failed to retrieve (transitive) usages of usage `{}`", iri)
+                }
+            },
+            pubgrub::PubGrubError::ErrorChoosingVersion { package, source } => match package {
+                DependencyIdentifier::Requested(_) => {
+                    match source {
+                        InternalSolverError::Resolution(err) => {
+                            anyhow!("resolution error: {:?}", err)
+                        }
+                        // InternalSolverError::InvalidProject => {
+                        //     anyhow!("found invalid project during usage resolution")
+                        // }
+                        InternalSolverError::NotResolvable(iri) => {
+                            anyhow!("unable to resolve usage `{}`", iri)
+                        }
+                        InternalSolverError::VersionNotAvailable(msg) => {
+                            anyhow!("requested version unavailable: {msg}")
+                        }
+                    }
+                    // anyhow!("unexpected internal error: {:?}", source)
+                }
+                DependencyIdentifier::Remote(iri) => {
+                    anyhow!("unable to select version of usage `{iri}`: {source}")
+                }
+            },
+            pubgrub::PubGrubError::ErrorInShouldCancel(err) => match err {
+                InternalSolverError::Resolution(err) => {
+                    anyhow!("resolution error: {:?}", err)
+                }
+                // InternalSolverError::InvalidProject => {
+                //     anyhow!("found invalid project during usage resolution")
+                // }
+                InternalSolverError::NotResolvable(iri) => {
+                    anyhow!("unable to resolve usage `{}`", iri)
+                }
+                InternalSolverError::VersionNotAvailable(msg) => {
+                    anyhow!("requested version unavailable: {msg}")
+                }
+            },
         }
-        Err(err) => Err(err)?,
-    };
-
-    wrapfs::write(
-        path.as_ref().join(DEFAULT_LOCKFILE_NAME),
-        lock.canonicalize().to_string(),
-    )?;
-
-    Ok(())
+    } else {
+        anyhow!("{lock_error}")
+    }
 }
