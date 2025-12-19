ed705faf6cb68f50eef6caace14bf2c0d38c135a
victor.linroth.sensmetry <victor.linroth@sensmetry.com>
2025-12-19 08:19:58 +0100

    Stricter assert for test `sources_without_std`.
    Signed-off-by: victor.linroth.sensmetry <victor.linroth@sensmetry.com>

--- sysand/tests/cli_source.rs
+++ sysand/tests/cli_source.rs
@@ -7,7 +7,6 @@ use assert_cmd::prelude::*;
 // pub due to https://github.com/rust-lang/rust/issues/46379
 mod common;
 pub use common::*;
-use predicates::prelude::PredicateBooleanExt;
 
 #[test]
 fn list_sources() -> Result<(), Box<dyn std::error::Error>> {
@@ -185,9 +184,9 @@ fn sources_without_std() -> Result<(), Box<dyn std::error::Error>> {
     out.assert().success();
 
     let out = run_sysand_in(&path, ["sources"], None)?;
-    out.assert().success().stdout(
-        predicates::str::contains("src.sysml").and(predicates::str::contains("src_dep.sysml")),
-    );
+    out.assert().success().stdout(predicates::str::is_match(
+        "^.*src\\.sysml\n.*src_dep\\.sysml\n$",
+    )?);
 
     Ok(())
 }
