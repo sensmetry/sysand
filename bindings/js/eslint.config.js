8eaed282c8a74eb3ed9e5ead9514dd012170a37a
victor.linroth.sensmetry <victor.linroth@sensmetry.com>
2026-02-04 00:41:46 +0100

    Fix Javascript.
    Signed-off-by: victor.linroth.sensmetry <victor.linroth@sensmetry.com>

--- bindings/js/eslint.config.js
+++ bindings/js/eslint.config.js
@@ -2,8 +2,6 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
-
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
