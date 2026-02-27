f8126ae95aad07436406e6594d5959187652ae1e
Erik Sundell <erik.sundell@sensmetry.com>
2026-02-27 14:25:05 +0100

    pre-commit: format js files
    Signed-off-by: Erik Sundell <erik.sundell@sensmetry.com>
    Signed-off-by: Erik Sundell <erik.sundell+2025@sensmetry.com>

--- bindings/js/webpack-browser-test.config.cjs
+++ bindings/js/webpack-browser-test.config.cjs
@@ -1,30 +1,30 @@
-const path = require('path');
+const path = require("path");
 //const HtmlWebpackPlugin = require('html-webpack-plugin');
 //const webpack = require('webpack');
 const glob = require("glob");
 
 module.exports = {
-    entry: glob.sync("spec/browser/*.spec.js").map((x) => './' + x), //.concat(["./shiv.js"]),
-    output: {
-        path: path.resolve(__dirname, 'browser_test_dist'),
-        filename: 'bundle.js',
+  entry: glob.sync("spec/browser/*.spec.js").map((x) => "./" + x), //.concat(["./shiv.js"]),
+  output: {
+    path: path.resolve(__dirname, "browser_test_dist"),
+    filename: "bundle.js",
+  },
+  // plugins: [
+  //     //new HtmlWebpackPlugin(),
+  //     new WasmPackPlugin({
+  //         crateDirectory: path.resolve(__dirname),
+  //         outDir: path.resolve(__dirname, "browser_pkg")
+  //     }),
+  // ],
+  resolve: {
+    modules: ["node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    alias: {
+      sysand$: path.resolve(__dirname, "browser_pkg/index.js"),
     },
-    // plugins: [
-    //     //new HtmlWebpackPlugin(),
-    //     new WasmPackPlugin({
-    //         crateDirectory: path.resolve(__dirname),
-    //         outDir: path.resolve(__dirname, "browser_pkg")
-    //     }),
-    // ],
-    resolve: {
-        modules: ["node_modules"],
-        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
-        alias: {
-            sysand$: path.resolve(__dirname, 'browser_pkg/index.js'),
-        },
-    },
-    mode: 'development', // mode: 'production'
-    experiments: {
-        asyncWebAssembly: true
-    }
+  },
+  mode: "development", // mode: 'production'
+  experiments: {
+    asyncWebAssembly: true,
+  },
 };
